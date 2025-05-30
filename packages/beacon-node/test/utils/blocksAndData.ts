b75c556233fb0b5a0ce55c4600609b45362d7742
matthewkeil <me@matthewkeil.com>
2025-05-30 21:59:49 +0700

    test: build util for creating batches and fix types

--- packages/beacon-node/test/utils/blocksAndData.ts
+++ packages/beacon-node/test/utils/blocksAndData.ts
@@ -1,8 +1,18 @@
+import {randomBytes} from "node:crypto";
 import {createChainForkConfig, defaultChainConfig} from "@lodestar/config";
-import {ForkPostCapella, ForkPostDeneb} from "@lodestar/params";
+import {
+  BYTES_PER_FIELD_ELEMENT,
+  FIELD_ELEMENTS_PER_BLOB,
+  ForkPostCapella,
+  ForkPostDeneb,
+  isForkPostDeneb,
+} from "@lodestar/params";
 import {computeStartSlotAtEpoch, signedBlockToSignedHeader} from "@lodestar/state-transition";
 import {SignedBeaconBlock, deneb, ssz} from "@lodestar/types";
 import {toRootHex} from "@lodestar/utils";
+import {VersionedHashes} from "../../src/execution/index.js";
+import {computeInclusionProof, kzgCommitmentToVersionedHash} from "../../src/util/blobs.js";
+import {ckzg} from "../../src/util/kzg.js";
 
 export const CAPELLA_FORK_EPOCH = 0;
 export const DENEB_FORK_EPOCH = 1;
@@ -29,9 +39,16 @@ export type BlockTestSet<F extends ForkPostCapella> = {
   rootHex: string;
 };
 
-export function buildBlockTestSet<F extends ForkPostCapella = ForkPostCapella>(forkName: F): BlockTestSet<F> {
+export function buildBlockTestSet<F extends ForkPostCapella = ForkPostCapella>(
+  forkName: F,
+  slot?: number,
+  parentRoot?: Uint8Array
+): BlockTestSet<F> {
   const block = ssz[forkName].SignedBeaconBlock.defaultValue();
-  block.message.slot = slots[forkName];
+  block.message.slot = slot ? slot : slots[forkName];
+  if (parentRoot) {
+    block.message.parentRoot = parentRoot;
+  }
   const blockRoot = ssz[forkName].BeaconBlock.hashTreeRoot(block.message as any);
   const rootHex = toRootHex(blockRoot);
   return {
@@ -53,8 +70,11 @@ export function buildParentAndChildBlockTestSet<F extends ForkPostCapella = Fork
   forkName: F
 ): ParentAndChildBlockTestSet<F> {
   const {block: parentBlock, blockRoot: parentBlockRoot, rootHex: parentRootHex} = buildBlockTestSet(forkName);
-  const {block: childBlock, blockRoot: childBlockRoot, rootHex: childRootHex} = buildBlockTestSet(forkName);
-  childBlock.message.slot = parentBlock.message.slot + 1;
+  const {
+    block: childBlock,
+    blockRoot: childBlockRoot,
+    rootHex: childRootHex,
+  } = buildBlockTestSet(forkName, parentBlock.message.slot + 1);
   childBlock.message.parentRoot = parentBlockRoot;
   return {
     parentBlock,
@@ -66,24 +86,58 @@ export function buildParentAndChildBlockTestSet<F extends ForkPostCapella = Fork
   };
 }
 
+export function generateBlobSidecars(
+  forkName: ForkPostDeneb,
+  block: SignedBeaconBlock<ForkPostDeneb>,
+  count: number
+): {blobSidecars: deneb.BlobSidecars; blobKzgCommitments: Uint8Array[]; versionedHashes: VersionedHashes} {
+  const blobKzgCommitments: Uint8Array[] = [];
+  const blobSidecars: deneb.BlobSidecars = [];
+
+  const signedBlockHeader = signedBlockToSignedHeader(config, block);
+  for (let index = 0; index < count; index++) {
+    const blobSidecar = ssz[forkName].BlobSidecar.defaultValue();
+    blobSidecar.index = index;
+    blobSidecar.signedBlockHeader = signedBlockHeader;
+    blobSidecar.blob = Uint8Array.from(randomBytes(FIELD_ELEMENTS_PER_BLOB * BYTES_PER_FIELD_ELEMENT));
+    blobSidecar.kzgCommitment = ckzg.blobToKzgCommitment(blobSidecar.blob);
+    blobSidecar.kzgCommitmentInclusionProof = computeInclusionProof(forkName, block.message.body, index);
+    blobSidecar.kzgProof = ckzg.computeBlobKzgProof(blobSidecar.blob, blobSidecar.kzgCommitment);
+
+    blobSidecars.push(blobSidecar);
+    blobKzgCommitments.push(blobSidecar.kzgCommitment);
+  }
+
+  const versionedHashes = blobKzgCommitments.map((commitment) => kzgCommitmentToVersionedHash(commitment));
+
+  return {
+    blobSidecars,
+    blobKzgCommitments,
+    versionedHashes,
+  };
+}
+
 export type BlockAndBlobTestSet<F extends ForkPostDeneb = ForkPostDeneb> = BlockTestSet<F> & {
   blobSidecars: deneb.BlobSidecars;
+  versionedHashes: VersionedHashes;
 };
-export function buildBlockAndBlobsTestSet(
-  forkName: ForkPostDeneb,
-  numberOfBlobs: number
-): BlockAndBlobTestSet<ForkPostDeneb> {
-  const {block, blockRoot, rootHex} = buildBlockTestSet<ForkPostDeneb>(forkName);
-  const commitments = Array.from({length: numberOfBlobs}, () => Buffer.alloc(48, 0x77));
-  block.message.body.blobKzgCommitments = commitments;
-  const signedBlockHeader = signedBlockToSignedHeader(config, block);
-  const blobSidecars: deneb.BlobSidecars = [];
-  for (const kzgCommitment of commitments) {
-    const blobSidecar = ssz[forkName].BlobSidecar.defaultValue();
-    blobSidecar.index = blobSidecars.length;
-    blobSidecar.signedBlockHeader = signedBlockHeader;
-    blobSidecar.kzgCommitment = kzgCommitment;
-    blobSidecars.push(blobSidecar);
+export function buildBlockAndBlobsTestSet<
+  F extends ForkPostCapella,
+  R extends F extends ForkPostDeneb ? BlockAndBlobTestSet<F> : BlockTestSet<F>,
+>(forkName: F, numberOfBlobs: number, slot?: number, parentRoot?: Uint8Array): R {
+  const {block, blockRoot, rootHex} = buildBlockTestSet<F>(forkName, slot, parentRoot);
+
+  let blobSidecars: undefined | deneb.BlobSidecars;
+  let versionedHashes: undefined | VersionedHashes;
+  if (isForkPostDeneb(forkName)) {
+    const {
+      blobKzgCommitments,
+      blobSidecars: blobs,
+      versionedHashes: hashes,
+    } = generateBlobSidecars(forkName, block as SignedBeaconBlock<ForkPostDeneb>, numberOfBlobs);
+    (block as SignedBeaconBlock<ForkPostDeneb>).message.body.blobKzgCommitments = blobKzgCommitments;
+    blobSidecars = blobs;
+    versionedHashes = hashes;
   }
 
   return {
@@ -91,5 +145,21 @@ export function buildBlockAndBlobsTestSet(
     blockRoot,
     rootHex,
     blobSidecars,
-  };
+    versionedHashes,
+  } as R;
+}
+
+export function buildBatchOfBlockWithBlobs<
+  F extends ForkPostCapella,
+  R extends F extends ForkPostDeneb ? BlockAndBlobTestSet<F> : BlockTestSet<F>,
+>(forkName: F, startSlot: number, count: number, minBlobs: number, maxBlobs: number): R[] {
+  const blocks: R[] = [];
+  let parentRoot = Uint8Array.from(randomBytes(32));
+  for (let slot = startSlot; slot < startSlot + count; slot++) {
+    const numberOfBlobs = Math.random() * (maxBlobs + 1 - minBlobs) + minBlobs;
+    const blockMaybeBlobs = buildBlockAndBlobsTestSet<F, R>(forkName, numberOfBlobs, slot, parentRoot);
+    parentRoot = blockMaybeBlobs.blockRoot;
+    blocks.push(blockMaybeBlobs as R);
+  }
+  return blocks;
 }
